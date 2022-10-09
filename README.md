1026fdc197aecc5e810ba7a5269f5783b21d3d1f
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-10-09 19:27:32 -0300

    Project concluded

---
+++ README.md
@@ -0,0 +1,4 @@
+# termux-test-app
+
+Repository created for authenticate github account in Tab P11 Plus
+
---
+++ autogit.sh
@@ -0,0 +1,7 @@
+git add .
+git commit -m "Project concluded"
+git branch -M main
+git remote add origin git@github.com:CarlosViniMSouza/termux-test-app.git
+git push -u origin main
+git status
+
